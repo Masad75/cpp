2c6a02665f37ca03abf9ee7384bb3774cb872524
NazmulHasanMasad <Nazmul.Hasan@g.bracu.ac.bd>
2024-03-11 22:02:11 +0600

    prime or not prime

---
+++ prime_or_not_prime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int i = 2; i < n; i++)
+    {
+
+        if (n % i == 0)
+        {
+
+            cout << n << "  is not a prime number   " << endl;
+            break;
+        }
+        else
+        {
+            cout << n << "   is  a prime number " << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
