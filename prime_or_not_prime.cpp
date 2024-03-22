905750968d910b9404035e0bbceb3f7c98efa12b
NazmulHasanMasad <Nazmul.Hasan@g.bracu.ac.bd>
2024-03-23 02:20:46 +0600

    prime or not prime

--- prime_or_not_prime.cpp
+++ prime_or_not_prime.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
 int main()
 {
-
     int n;
     cin >> n;
-
-    for (int i = 2; i < n; i++)
+    bool flag = 0;
+    for (int i = 2; i <= sqrt(n); i++)
     {
-
         if (n % i == 0)
         {
+            cout << "non prime" << endl;
+            flag = 1;
+            break;
+        }
+    }
 
-            cout << n << "  is not a prime number   " << endl;
-            break;
-        }
-        else
-        {
-            cout << n << "   is  a prime number " << endl;
-            break;
-        }
+    if (flag == 0)
+    {
+        cout << "prime" << endl;
     }
 
     return 0;
