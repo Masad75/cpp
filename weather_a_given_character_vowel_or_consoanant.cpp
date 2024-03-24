8c4d3757f68ecc0e5d3a70a2a191a7d96f682054
NazmulHasanMasad <Nazmul.Hasan@g.bracu.ac.bd>
2024-03-24 07:21:11 +0600

    vowel or consonant

---
+++ weather_a_given_character_vowel_or_consoanant.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char c;
+    cin >> c;
+
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+
+    {
+        cout << "vowel";
+    }
+    else
+    {
+        cout << " consonant ";
+    }
+
+    return 0;
+}
