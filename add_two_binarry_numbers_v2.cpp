33d0af4c92106e0c542a98155f78c8870b9c8f86
NazmulHasanMasad <Nazmul.Hasan@g.bracu.ac.bd>
2024-03-31 02:30:04 +0600

    add two binary numbersv3

--- add_two_binarry_numbers_v2.cpp
+++ add_two_binarry_numbers_v2.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 using namespace std;
+int addBinary(long int n1, long int n2)
+{
+    int i = 0, carry = 0, sum[16];
+
+    while (n1 != 0 || n2 != 0)
+    {
+        sum[i++] = (n1 % 10 + n2 % 10 + carry) % 2;
+        carry = (n1 % 10 + n2 % 10 + carry) / 2;
+        n1 = n1 / 10;
+        n2 = n2 / 10;
+    }
+
+    if (carry == 1)
+    {
+        sum[i++] = carry;
+    }
+    i--;
+
+    while (i >= 0)
+    {
+        cout << sum[i--];
+    }
+    return 0;
+}
 
 int main()
 {
-    long int bin_one, bin_two, t1, t2;
-    int i = 0, rem = 0, sum[16];
-    cout << "Enter First Binary Number: ";
-    cin >> bin_one;
-    cout << "Enter Second Binary Number: ";
-    cin >> bin_two;
-
-    t1 = bin_one;
-    t2 = bin_two;
-
-    while (bin_one != 0 || bin_two != 0)
-    {
-        sum[i++] = (bin_one % 10 + bin_two % 10 + rem) % 2;
-        rem = (bin_one % 10 + bin_two % 10 + rem) / 2;
-        bin_one = bin_one / 10;
-        bin_two = bin_two / 10;
-    }
-
-    if (rem != 0)
-        sum[i++] = rem;
-
-    i--;
-    cout << endl
-         << t1 << " + " << t2 << " = ";
-
-    while (i >= 0)
-        cout << sum[i--];
+    long int n1, n2;
+    cin >> n1 >> n2;
+    addBinary(n1, n2);
 
     return 0;
 }
